7a07e7e06d94379685090c8d6840eb21d96fdb22
Jacob1010-h <lordgaming1010@gmail.com>
2022-05-15 19:45:09 -0700

    everything is working and it uploads to google sheets but.., I CANT GET COUNT TO WORK

--- counterFunctions.js
+++ counterFunctions.js
@@ -1,16 +1,17 @@
 var val = setInterval(function() {
     document.getElementById("val").innerHTML = getVal();
-},10);
-
+},100);
+minus();
 
 function getVal(){
+    console.log(val);
     return val;
 }
 function plus() {
     val += 1;
-    console.log(getVal());
+    console.log(val + "++");
 }
 function minus(){
     val -= 1;
-    console.log(getVal());
+    console.log(val + "--");
 }
