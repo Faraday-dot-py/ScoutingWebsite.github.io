8d23324d6971486a449421574cd9cf1700c8bca5
Jacob1010-h <lordgaming1010@gmail.com>
2022-07-01 14:39:46 -0700

    AHHHHHHHHHHHHHHHHHHH
    vs code wasnt allowing me to do things so idk.... BUT IT FIXED!!

--- sketch.js
+++ sketch.js
@@ -1,56 +1,68 @@
-var txt;
-var newLine = document.createElement('br');
-
+var txt
 
 function preload() {
-    txt = loadStrings("settings.txt");
+  txt = loadStrings("settings.txt");
 }
 
 function getElements(foo) {
     let element = foo.split(";");
+    let text = "";
     let onClick = false;
 
     if(element[0] === "button") {
         createButtonElem(element, text, onClick);
     }
-    if(element[0] === "radio"){
-        createRatioElem(element);
-    }
-//    if(element[0] === "counter"){
-//        console.log("THIS IS TOTALLY A COUNTER");
-//        createCounter();
-//    }
 }
 
-function createRatioElem(elemArray) {
-    var radioText;
-    let params = elemArray[1].split(":");
-    replaceSpaces(params);
-    for(let i=0; i<params.length; i++) {
-        if(params[i] == "options"){
-            radioText = splitCommas(params[i+1]);
-            console.log(radioText);
-        }
-    }
-    var form = document.getElementById('form');
-    form.appendChild(newLine);
-    radioText.forEach(text => {
-        createRatio(text);
-    });
-    //WORK ON RADIO (CREATE RADIO FUNCTION)
-}
-function createButtonElem(elemArray, text, onClick) {
+function createButtonElem(elemArray, buttonText, onClick) {
     let params = elemArray[1].split(":");
     replaceSpaces(params);
     for(let i=0; i<params.length; i++) {
         if(params[i] == "text"){
-            text = params[i+1];
+            buttonText = params[i+1];
         }else if(params[i] == "increment") {
             if(params[i+1] == "true") {
                 onClick = true;
             }
+            console.log(params.length-1 - i);
         }
     }
+    console.log("button text: " + buttonText);
+    console.log("onClick result: " + onClick);
+    console.log(params);
+    writeButton(buttonText, onClick);
+}
+
+function createButtonPlus(text) {
+    var buttonPlus = document.createElement('button');
+        buttonPlus.innerHTML = text;
+        buttonPlus.onclick = function() {
+            plus();
+            updateVal();
+        }
+    var form = document.getElementById('form');
+    form.appendChild(buttonPlus);
+}
+
+function createButtonMinus(text) {
+    var buttonMinus = document.createElement('button');
+        buttonMinus.innerHTML = text;
+        buttonMinus.onclick = function() {
+            minus();
+            updateVal();
+        }
+    var form = document.getElementById('form');
+    form.appendChild(buttonMinus);
+}
+
+function createButtonOther(text) {
+    var buttonOther = document.createElement('button');
+        buttonOther.innerHTML = text;
+    var form = document.getElementById('form');
+    form.appendChild(buttonOther);
+}
+
+function writeButton(text, onClick) {
     if(text != "" && onClick == true) {
         if(text == "+"){
             createButtonPlus(text);
@@ -63,66 +75,20 @@ function createButtonElem(elemArray, text, onClick) {
         createButtonOther(text);
     }
 }
-//function createCounter(){
-////    var buttonPlus = document.createElement('button');
-////    buttonPlus.innerHTML = "+";
-////    buttonPlus.onclick = function() {
-////        plus();
-////        updateVal();
-////    }
-//   var form = document.getElementById('form');
-////    form.appendChild(buttonPlus);
-//    var input = document.createElement('input');
-//    input.setAttribute('placeholder', "0");
-//    input.setAttribute('readonly', true);
-//    form.appendChild(input);
-//    form.appendChild(newLine);
-//}
 
-function createButtonPlus(text) {
-    plus_button = createButton(text);
-    plus_button.mouseClicked(plusFunc);
-}
-function createButtonMinus(text) {
-    minus_button = createButton(text);
-    minus_button.mouseClicked(minusFunc);
-}
-function createButtonOther(text) {
-    other_button = createButton(text);
-}
-function createRatio(text){
-    var labelValue = document.createElement('label');
-    labelValue.innerHTML = " : " + text;
-    var inputValue = document.createElement('input');
-    inputValue.type = "radio";
-    inputValue.name = "text";
-    var form = document.getElementById('form');
-    form.appendChild(inputValue);
-    form.appendChild(labelValue);
-}
-
-function minusFunc(){
-    minus();
-    updateVal();
-}
-function plusFunc(){
-    plus();
-    updateVal();
-}
 function replaceSpaces(array) {
     for(let i=0; i<array.length; i++) {
         array[i] = array[i].replace(/\s/g, '');
     }
 }
-function splitCommas(array){
-    array = array.split(",");
-    return array;
+
+function setup() {
+  noCanvas();
+  txt.forEach(elementRaw => {
+      getElements(elementRaw);
+  });
 }
 
-function setup(){
-    noCanvas();
-    txt.forEach(elementRaw => {
-//        createP(elementRaw);
-        getElements(elementRaw);
-    });
-}
+// function draw() {
+//   background(220);
+// }
